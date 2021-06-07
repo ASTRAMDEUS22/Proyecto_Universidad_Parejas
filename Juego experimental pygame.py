2f0b2204cebfc48f118f0d4eac68263415e16579
ASTRAMDEUS22 <josthin.soto.s@gmail.com>
2021-06-07 17:09:34 -0600

    Añadí que las balas desaparezcan
    Hice que las balas desaparezcan al tocar la nave del jugador

--- Juego experimental pygame.py
+++ Juego experimental pygame.py
@@ -9,14 +9,15 @@ Largo , Alto = 1366,768
 SCREEN = pygame.display.set_mode((Largo,Alto))
 
 #LUGAR DE APARICION DE LAS NAVES
-spawn_X = random.randrange(1,1200)
+spawn_X = random.randrange(1,180)
 spawn_Y = random.randrange(1,600)
 #DIRECCION DE LA BALA POSS CHOQUE
 Up_down_direction = random.randint(1,3)
 
 Right_left_direction = random.randint(1,3)
 
-
+Direccion_aleatoria_X = random.randrange(1,10)
+Direccion_aleatoria_Y = random.randrange(1,10)
 #TITULO DE LA VENTANA
 
 
@@ -120,12 +121,12 @@ class Enemigo(pygame.sprite.Sprite):
         # Se obtiene el rectangulo
         self.rect = self.image.get_rect()
         # ACOMODAR EL RECTANGULO/CENTRO
-        self.rect.center = (spawn_X, spawn_Y)
+        self.rect.center = (100, 300)
         self.rect.x = random.randrange(Largo - self.rect.width)
         self.rect.y = random.randrange(Alto - self.rect.height)
 
-        self.acceleracion_inicialX = random.randint(4,6)
-        self.acceleracion_inicialY = random.randint(4,6)
+        self.acceleracion_inicialX = 1
+        self.acceleracion_inicialY = 1
 
     def update(self):
 
@@ -136,58 +137,50 @@ class Enemigo(pygame.sprite.Sprite):
         # LIMITAR EL BORDE  IZQUIERDO
         if self.rect.left <= 0:
             if Right_left_direction == 1:#CON ESTE LA BALA BAJA HACIA LA DERECHA
-                self.acceleracion_inicialX = random.randint(4,6)
-                self.acceleracion_inicialY = random.randint(4,6)
+                self.acceleracion_inicialX = Direccion_aleatoria_X
+                self.acceleracion_inicialY = Direccion_aleatoria_Y
 
             elif Right_left_direction == 2:#CON ESTE LA BALA SUBE HACIA LA DERECHA
-                self.acceleracion_inicialX = random.randint(4,6)
-                self.acceleracion_inicialY = -random.randint(4,6)
+                self.acceleracion_inicialX = Direccion_aleatoria_X
+                self.acceleracion_inicialY = -Direccion_aleatoria_Y
 
-            elif Right_left_direction == 3:
-                self.acceleracion_inicialX = random.randint(4,6)
-                self.acceleracion_inicialY = 0
+
 
         # LIMITAR EL BORDE DERECHO
         elif self.rect.right >= Largo:
             if Right_left_direction == 1:#CON ESTE LA BALA BAJA HACIA LA IZQUIERDA
-                self.acceleracion_inicialX = -random.randint(4,6)
-                self.acceleracion_inicialY = random.randint(4,6)
+                self.acceleracion_inicialX = -Direccion_aleatoria_X
+                self.acceleracion_inicialY = Direccion_aleatoria_Y
 
             elif Right_left_direction == 2:#CON ESTE LA BALA SUBE HACIA LA IZQUIERDA
-                self.acceleracion_inicialX = -random.randint(4,6)
-                self.acceleracion_inicialY = -random.randint(4,6)
+                self.acceleracion_inicialX = -Direccion_aleatoria_X
+                self.acceleracion_inicialY = -Direccion_aleatoria_Y
 
-            elif Right_left_direction == 3:
-                self.acceleracion_inicialX = -random.randint(4,6)
-                self.acceleracion_inicialY = 0
+
 
         # LIMITAR EL BORDE INFERIOR
         elif self.rect.bottom >= Alto:#CON ESTE LA BALA SUBE HACIA LA DERECHA
             if Up_down_direction == 1:
-                self.acceleracion_inicialX = random.randint(4,6)
-                self.acceleracion_inicialY = -random.randint(4,6)
+                self.acceleracion_inicialX = Direccion_aleatoria_X
+                self.acceleracion_inicialY = -Direccion_aleatoria_Y
 
             elif Up_down_direction == 2:#CON ESTE LA BALA SUBE HACIA LA IZQUIERDA
-                self.acceleracion_inicialX = -random.randint(4,6)
-                self.acceleracion_inicialY = -random.randint(4,6)
+                self.acceleracion_inicialX = -Direccion_aleatoria_X
+                self.acceleracion_inicialY = -Direccion_aleatoria_Y
 
-            elif Up_down_direction == 3:
-                self.acceleracion_inicialX = 0
-                self.acceleracion_inicialY = -random.randint(4,6)
+
 
         # LIMITAR EL BORDE  SUPERIOR
         elif self.rect.top <= 0:
             if Up_down_direction == 1:#CON ESTE LA BALA BAJA HACIA LA DERECHA
-                self.acceleracion_inicialX = random.randint(4,6)
-                self.acceleracion_inicialY = random.randint(4,6)
+                self.acceleracion_inicialX = Direccion_aleatoria_X
+                self.acceleracion_inicialY = Direccion_aleatoria_Y
 
             elif Up_down_direction == 2:#CON ESTE LA BALA BAJA HACIA LA IZQUIERDA
-                self.acceleracion_inicialX = -random.randint(4,6)
-                self.acceleracion_inicialY = random.randint(4,6)
+                self.acceleracion_inicialX = -Direccion_aleatoria_X
+                self.acceleracion_inicialY = Direccion_aleatoria_Y
 
-            elif Up_down_direction == 3:
-                self.acceleracion_inicialX = 0
-                self.acceleracion_inicialY = random.randint(4,6)
+
 
 #_____________________________________________CREACION DE LA VENTANA_____________________________________________________#
 pygame.init()
@@ -199,28 +192,30 @@ clock = pygame.time.Clock()
 #_______________________________________________SPRITES______________________________________________________________
 
 sprites = pygame.sprite.Group()
-Enemigos = pygame.sprite.Group()
+Enemigoss =pygame.sprite.Group()
+
+
 
 
 #SPRITE ENEMIGOS
 Bullets = Enemigo()
-sprites.add((Bullets))
+Enemigoss.add((Bullets))
 
 Bullets2 = Enemigo()
-sprites.add((Bullets2))
+Enemigoss.add((Bullets2))
 
 Bullets3 = Enemigo()
-sprites.add((Bullets3))
+Enemigoss.add((Bullets3))
 '''
 Bullets4 = Enemigo()
-sprites.add((Bullets4))'''
-'''
+Enemigoss.add((Bullets4))
+
 Bullets5 = Enemigo()
-sprites.add((Bullets5'''
-'''
+sprites.add((Bullets5))
+
 Bullets6 = Enemigo()
-sprites.add((Bullets6))'''
-'''
+sprites.add((Bullets6))
+
 Bullets7 = Enemigo()
 sprites.add((Bullets7))'''
 #SPRITE JUGADOR
@@ -285,7 +280,7 @@ def actualizacionPantalla():
 
 #BUCLE PARA LA VENTANA
 Dios = True
-Aleatorio = True
+
 while Dios:
     #FPS
     RELOJ.tick(100)
@@ -326,13 +321,16 @@ while Dios:
     if keys[pygame.K_DOWN] and py < 610:
         py += velocidad
 
+    print("--------------NUMEROS ALEATORIOS GENERADOS-------------")
     Up_down_direction = random.randint(1, 3)
     Right_left_direction = random.randint(1, 3)
     print(Up_down_direction)
     print(Right_left_direction)
-
-
-
+    Direccion_aleatoria_X = random.randint(4, 8)
+    Direccion_aleatoria_Y = random.randint(4, 8)
+    print(Direccion_aleatoria_X)
+    print(Direccion_aleatoria_Y)
+    print("--------------NUMEROS ALEATORIOS GENERADOS-------------")
 
 #___________________________________________ZONA DE VENTANA________________________________________________#
     #ACTUALIZACION DE LA VENTANA
@@ -341,14 +339,20 @@ while Dios:
 
     # Actualizar los sprites
     sprites.update()
-    Enemigos.update()
+    Enemigoss.update()
+
+    #COLISIONES
+    Colission = pygame.sprite.spritecollide(Jugador, Enemigoss, True)
+
+
     # PERSONALIZACION
 
     sprites.draw(pantalla)
+    Enemigoss.draw(pantalla)
 
     # REFRESCA LA PANTALLA
     pygame.display.flip()
 
-print("NUMEROS RANDOM GENERADOS")
+
 
 pygame.quit()
