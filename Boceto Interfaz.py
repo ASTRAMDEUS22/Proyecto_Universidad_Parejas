951b76f6c8d9b99ad02f5453da8608150e43b7a6
AxelFloresLara <85202715+AxelFloresLara@users.noreply.github.com>
2021-06-07 17:03:06 -0600

    Primer Boceto interfaz
    Añado por primera vez el intento de Interfaz

---
+++ Boceto Interfaz.py
@@ -0,0 +1,40 @@
+import pygame_menu
+from pygame_menu.examples import create_example_window
+import pygame
+from typing import Tuple, Any
+
+surface = create_example_window('PRUEBA DE INTERFAZ', (500, 400))
+
+
+def set_difficulty(selected: Tuple, value: Any) -> None:
+    """
+    Set the levels to play.
+    
+    """
+    print('Seleccionar dificultad {} ({})'.format(selected[0], value))
+
+
+def start_the_game() -> None:
+    """
+    Function that starts a game.
+    """
+    global user_name
+    print('{0}, AQUI VA EL JUEGO'.format(user_name.get_value()))
+
+
+menu = pygame_menu.Menu(
+    height=400,
+    theme=pygame_menu.themes.THEME_SOLARIZED,
+    title='¡BIENVENIDO!',
+    width=500
+)
+
+user_name = menu.add.text_input('Nombre: ', default='', maxchar=10)
+menu.add.selector('Nivel: ', [('Primero', 1), ('Segundo', 2),('Tercero', 3)], onchange=set_difficulty)
+menu.add.button('Jugar', start_the_game)
+menu.add.button('creditos', start_the_game, width=500, height=600)
+menu.add.button('Salir', pygame_menu.events.EXIT)
+
+if __name__ == '__main__':
+    menu.mainloop(surface)
+
